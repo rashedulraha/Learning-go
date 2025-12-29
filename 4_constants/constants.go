62a43c5592b27f4bf4678f3a38d4dc44ad965578
Rashedul Islam <rashedulraha.bd@gmail.com>
2025-12-29 20:45:19 +0600

    adding constants and for

--- 4_constants/constants.go
+++ 4_constants/constants.go
@@ -6,4 +6,12 @@ import "fmt"
 
 	const age= 45
 	fmt.Println(age)
+
+
+	const (
+		port=5000
+		host ="localhost"
+	)
+
+	fmt.Println(port, host)
  }
---
+++ 5_for/for.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+// for only construct in go  for looping
+ func main (){
+
+
+	// while looping
+
+	i:=1
+	 for i <=3{
+		fmt.Println(i)
+		i=i+1
+	 }
+
+
+ }
