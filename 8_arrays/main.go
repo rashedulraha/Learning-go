a4d51c87dfedff33c7b07f5eb1d0c7cc29d98cf9
Rashedul Islam <rashedulraha.bd@gmail.com>
2025-12-30 17:51:19 +0600

    array

--- 8_arrays/main.go
+++ 8_arrays/main.go
@@ -14,15 +14,38 @@ import "fmt"
 
 
 
-	  // check bool
+	  //! check bool
 
-		 var bools [4]bool
-		  bools[2]=true
-		 fmt.Println(bools)
+		//  var bools [4]bool
+		//   bools[2]=true
+		//  fmt.Println(bools)
 
 
-		//  check string
-		var checkString  [4]string
-		checkString[2]="Check name"
-		fmt.Println(checkString)
+		//!  check string
+		// var checkString  [4]string
+		// checkString[2]="Check name"
+		// fmt.Println(checkString)
+
+
+		//!  to declare it in single line
+		// number:=[4]int{1,2,3,4}
+
+		// fmt.Println(number)
+
+
+		//! 2d array 
+
+		// number:= [2][2] int{{1,2},{5,6}}
+		// fmt.Println(number)
+
+
+		//  - fixed size predictable
+		//  - memory optimization
+		//  - constant time
+		
+
+
+
+
+
  }
