4ba25753c8c99f6f88efa96bb7a5212c629f833d
Rashedul Islam <rashedulraha.bd@gmail.com>
2025-12-30 17:40:51 +0600

    array string number

--- 8_arrays/main.go
+++ 8_arrays/main.go
@@ -11,4 +11,18 @@ import "fmt"
 	 fmt.Println(num[0])
 	 fmt.Println(num)
 
+
+
+
+	  // check bool
+
+		 var bools [4]bool
+		  bools[2]=true
+		 fmt.Println(bools)
+
+
+		//  check string
+		var checkString  [4]string
+		checkString[2]="Check name"
+		fmt.Println(checkString)
  }
