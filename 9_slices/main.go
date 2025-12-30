59d0bd3296a682e011bfb97b4a53d840928d4e59
Rashedul Islam <rashedulraha.bd@gmail.com>
2025-12-31 03:33:32 +0600

    learning slise and other other fuild in feature

--- 9_slices/main.go
+++ 9_slices/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 // slices -> dynamic
 // most use constructs in  go
 // + useful method
@@ -8,52 +10,60 @@ package main
 
 	// uninitialized slices is nil
 
-	// var name []int
-	// fmt.Println(name==nil)
+	var name []int
+	fmt.Println(name==nil)
 
-	// var number =make([]int, 6 ,50)
-	// fmt.Println(cap(number))
-
-
-	// number = append(number, 5)
-	// fmt.Println(cap(number))
-	// fmt.Println(number)
+	var number =make([]int, 6 ,50)
+	fmt.Println(cap(number))
 
 
-	//  fmt.Println(number == nil)
+	number = append(number, 5)
+	fmt.Println(cap(number))
+	fmt.Println(number)
+
+
+	 fmt.Println(number == nil)
 
 
 	//! copy function
 
-	//  var number = make([]int, 0, 5)
-	//  number =append(number, 1)
-	//  var number2  =make([]int, len(number))
+	 var number = make([]int, 0, 5)
+	 number =append(number, 1)
+	 var number2  =make([]int, len(number))
 
 
-	//  copy(number2,number)
+	 copy(number2,number)
 
-	//  fmt.Println(number,number2)
+	 fmt.Println(number,number2)
 
 
 
 	//  ! slice operator 
 
-	// var number   =[]int{1,2,3,4,5}
-	// // fmt.Println(number[2:4])
-	// // fmt.Println(number[2])
-	// // fmt.Println(number[:2])
-	// fmt.Println(number[1:])
+	var number   =[]int{1,2,3,4,5}
+	// fmt.Println(number[2:4])
+	// fmt.Println(number[2])
+	// fmt.Println(number[:2])
+	fmt.Println(number[1:])
 
 
 
-	//  slices in build feature 
-	//  var number1 = []int{1,2, 5}
-	//  var number2  = []int{1,2 , 4}
+	 slices in build feature 
+	 var number1 = []int{1,2, 5}
+	 var number2  = []int{1,2 , 4}
 
 
-	//  fmt.Println(slices.Equal(number1,number2))
+	 fmt.Println(slices.Equal(number1,number2))
 
 
+
+	//  slice 2d array 
+
+	 var array  =[][]int{{3,4,5},{6,7,8}}
+
+	 fmt.Println(array)
+
+ 
 	
 
 
