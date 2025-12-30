899119d62142fa54ccd12748ae87e7e2a5fb2539
Rashedul Islam <rashedulraha.bd@gmail.com>
2025-12-30 12:55:14 +0600

    add classic looping and pringln to for lopping

--- 5_for/for.go
+++ 5_for/for.go
@@ -1,18 +1,31 @@
 package main
 
-import "fmt"
-
 // for only construct in go  for looping
  func main (){
 
 
 	// while looping
 
-	i:=1
-	 for i <=3{
-		fmt.Println(i)
-		i=i+1
-	 }
+	// i:=1
+	//  for i <=3{
+	// 	fmt.Println(i)
+	// 	i=i+1
+	//  }
 
+
+
+	//   infinite loop 
+
+	// for{
+	// 	println(1)
+	// }
+
+
+	//  classic for loop 
+	 for  i:= 0 ; i<=3; i++{
+
+		println(i)
+
+	 }
 
  }
