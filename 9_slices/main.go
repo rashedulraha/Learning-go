324cfdc5dff9e94a6e2af656415905b4cb0181e3
Rashedul Islam <rashedulraha.bd@gmail.com>
2025-12-30 18:25:57 +0600

    slies and make

---
+++ 9_slices/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+// slices -> dynamic
+// most use constructs in  go
+// + useful method
+
+ func main(){
+
+	// uninitialized slices is nil
+
+	// var name []int
+	// fmt.Println(name==nil)
+
+	var number =make([]int, 4)
+
+	 fmt.Println(number == nil)
+
+ }
