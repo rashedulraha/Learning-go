f8cf1546c7af29615137ecedd5718dc7237463ca
Rashedul Islam <rashedulraha.bd@gmail.com>
2025-12-31 14:57:23 +0600

    function and result to println return value

--- 12_Function/function.go
+++ 12_Function/function.go
@@ -2,13 +2,33 @@ package main
 
 import "fmt"
 
-func add(a int, b int) int {
+// func add(a int, b int) int {
+// 	return a + b
+// }
+
+//  string  function
+
+
+ func stringValue ()(string,string,string){
+	return "Javascript" , "typescript", "next.js"
+ }
+
+//?  feature
+func add(a, b int) int {
 	return a + b
 }
 
 func main() {
 
-	result := add(5, 5)
+	// result := add(5, 4)
+	// fmt.Println(result)
 
-	fmt.Println(result)
+	//?  call string value function
+
+
+	 valueOne , valueTwo , valueThree  := stringValue()
+	 fmt.Println(valueOne , valueTwo, valueThree )
+
+	//  fmt.Println(stringValue())
+
 }
