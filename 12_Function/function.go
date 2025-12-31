18b77aa21ff9e77680e6358ba29d99c151b64208
Rashedul Islam <rashedulraha.bd@gmail.com>
2025-12-31 15:08:48 +0600

    function

--- 12_Function/function.go
+++ 12_Function/function.go
@@ -1,24 +1,34 @@
 package main
 
-import "fmt"
-
 // func add(a int, b int) int {
 // 	return a + b
 // }
 
 //  string  function
 
-
- func stringValue ()(string,string,string){
-	return "Javascript" , "typescript", "next.js"
- }
+//  func stringValue ()(string,string,string){
+// 	return "Javascript" , "typescript", "next.js"
+//  }
 
 //?  feature
-func add(a, b int) int {
-	return a + b
+// func add(a, b int) int {
+// 	return a + b
+// }
+
+
+func processIt (fn func(a int)int){
+	fn(1)
 }
 
+
 func main() {
+
+	fn:=func (a int) int {
+		return 2
+		
+	}
+
+	processIt(fn)
 
 	// result := add(5, 4)
 	// fmt.Println(result)
@@ -26,8 +36,8 @@ func main() {
 	//?  call string value function
 
 
-	 valueOne , valueTwo , valueThree  := stringValue()
-	 fmt.Println(valueOne , valueTwo, valueThree )
+	//  valueOne , valueTwo , valueThree  := stringValue()
+	//  fmt.Println(valueOne , valueTwo, valueThree )
 
 	//  fmt.Println(stringValue())
 
