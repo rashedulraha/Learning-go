3925959536870e678ec055ee848653c8033d2903
Rashedul Islam <rashedulraha.bd@gmail.com>
2025-12-31 17:13:39 +0600

    ponter location

--- 15_Pointers/pointers.go
+++ 15_Pointers/pointers.go
@@ -2,18 +2,26 @@ package main
 
 import "fmt"
 
-func changeNum(num int) {
-	num = 5
+// func changeNum(num int) {
+// 	num = 5
 
-	fmt.Println("In change Number", num)
+// fmt.Println("In change Number", num)
+// }
+
+// by reference
+
+func changeNum(number *int){
+	*number = 45
+	fmt.Println("In changeNum" , *number)
 
 }
+
 
 func main() {
 
 
-	num:=1
-	changeNum(num)
-	fmt.Println("After change in main function" , num)
-
+	num:=57
+	changeNum(&num)
+	fmt.Println("memory address" , num)
+	// fmt.Println("After change in main function" , num)
 }
