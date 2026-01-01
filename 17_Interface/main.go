8f095ffe11beca42a02b75f208f9c7d4b8f0bac2
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-01-01 11:58:43 +0600

    remove razorpay and add stripe

--- 17_Interface/main.go
+++ 17_Interface/main.go
@@ -6,8 +6,13 @@ type payment struct{}
 
 func (p payment) makePayment(amount float32) {
 
-	razorpayPaymentGW:=razorpay{}
-	razorpayPaymentGW.pay(amount)
+	// razorpayPaymentGW:=razorpay{}
+	// razorpayPaymentGW.pay(amount)
+
+	// ! payment stripe 
+
+	 stripePaymentGW:=stripe{}
+	 stripePaymentGW.pay(amount)
 
 
 }
@@ -16,14 +21,20 @@ type razorpay struct{}
 
 func (r razorpay) pay(amount float32) {
 	// logic to make payment
-
 	fmt.Println("making payment using razorpay" , amount)
-
 }
 
+// payment stript 
+
+ type stripe struct{}
+ func (s stripe) pay(amount float32 ){
+	// logic to make payment
+	fmt.Println("making payment using stripe" , amount)
+
+ }
 func main() {
 
 	newPayment:=payment{}
-	newPayment.makePayment(200)
+	newPayment.makePayment(2500)
 
 }
