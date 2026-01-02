c9ef1616515fa6bcb7076ea60748c32821abdcbb
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-01-02 21:21:11 +0600

    generic funciton

--- 19_Generics/main.go
+++ 19_Generics/main.go
@@ -4,23 +4,27 @@ import "fmt"
 
 // print slice
 
-  func printSlice(items []int){
+  func printSlice[T int | string](items []T){
 		for _,item :=range items{
 			fmt.Println(item)
 		}
 	}
-  func printStringSlice(items []string){
-		for _,item :=range items{
-			fmt.Println(item)
-		}
-	}
+  // func printStringSlice(items []string){
+	// 	for _,item :=range items{
+	// 		fmt.Println(item)
+	// 	}
+	// }
 
 func main() {
 	// fmt.Println("Hello Generics")
 
-	// number:=[]int{1,2,3,4,5,6,7,8,9}
+	number:=[]int{1,2,3,4,5,6,7,8,9}
 	// printSlice(number)
 
-	PersonName :=[]string{"Rashedul Islam" , "Abdullah" ,"Rabbi"}
-	printStringSlice(PersonName)
+	// PersonName :=[]string{"Rashedul Islam" , "Abdullah" ,"Rabbi","Nothing"}
+	// values:=[]bool{true , false }
+	printSlice(number)
+
+
+
 }
