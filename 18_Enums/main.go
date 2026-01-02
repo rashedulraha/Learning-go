789db895f802ac8fee4a4430ee769648db33eac4
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-01-02 20:47:58 +0600

    update enum page

--- 18_Enums/main.go
+++ 18_Enums/main.go
@@ -2,6 +2,24 @@ package main
 
 import "fmt"
 
+//!   my type string
+// type myType string
+	type orderStatus string
+
+
+	 const(
+
+		received orderStatus = "Received"
+			Confirmed = "Confirmed"
+			Prepared ="Prepared"
+			Delivered ="Delivered"
+	 )
+
+
+ func changeOrderStatus(status orderStatus){
+	fmt.Println("changing order status to" ,status)
+ }
+
 func main() {
-	fmt.Println("Hello world")
+changeOrderStatus(Delivered)
 }
