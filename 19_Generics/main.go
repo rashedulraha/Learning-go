7e2e46aea8bed52aaceb76239822728b17bd2ee4
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-01-02 21:13:19 +0600

    pas string to print name

--- 19_Generics/main.go
+++ 19_Generics/main.go
@@ -9,9 +9,18 @@ import "fmt"
 			fmt.Println(item)
 		}
 	}
+  func printStringSlice(items []string){
+		for _,item :=range items{
+			fmt.Println(item)
+		}
+	}
 
 func main() {
 	// fmt.Println("Hello Generics")
 
-	printSlice([]int{1,2,3,4,5,6,7,8,9})
+	// number:=[]int{1,2,3,4,5,6,7,8,9}
+	// printSlice(number)
+
+	PersonName :=[]string{"Rashedul Islam" , "Abdullah" ,"Rabbi"}
+	printStringSlice(PersonName)
 }
