e860c1becd9a5275b4577ebd32c56dd6d9f26438
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-01-06 12:37:18 +0600

    constants

---
+++ Learning-Go-2/8_constants/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	const appName = "Go basic"
+	const maxUpload int = 45
+	const discountPrice float32 = 20.2
+
+	fmt.Println(appName, maxUpload, discountPrice)
+
+}
