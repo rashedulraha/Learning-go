c5019de5f0121d4b8892158e9966e5bb70bc1b66
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-01-06 11:58:11 +0600

    adding go basic and variable

---
+++ Learning-Go-2/2_variables/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	// string
+	var name string = "Rashedul islam"
+
+	 // integer
+
+	  var number  int  = 45
+
+		//  
+
+
+	fmt.Println(name)
+	fmt.Println(number)
+
+}
---
+++ Learning-Go-2/3_Package/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+
+	// Package name . function name
+
+	fmt.Println("sqrt(25)",math.Sqrt(25))
+}
