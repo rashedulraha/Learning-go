e9d9e5b9417895d5da06e0072f8a9301565d0fbb
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-01-06 12:07:43 +0600

    var and short

---
+++ Learning-Go-2/4_var_short/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var name string
+	// name = "Rashedul Islam"
+
+	//var age = 54 // inferred to int
+
+	// :=
+
+	number := 45
+	number = number + 45
+
+	fmt.Println(number)
+
+}
