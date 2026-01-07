6c9faeb5153b52483282338953eaacf9447d52a6
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-01-07 20:06:28 +0600

    array  and array literal

---
+++ Learning-Go-2/13_Arrary/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	var marks [3] int
+
+	marks[0] = 10
+	marks[1] = 50
+	marks[2] = 60
+
+	fmt.Println(marks)
+
+
+
+	//  array literal 
+	res :=[5]int{20,50,50,50,05,}
+	fmt.Println(res)
+
+}
