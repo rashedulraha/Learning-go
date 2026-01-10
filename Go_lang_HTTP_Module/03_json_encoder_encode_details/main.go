008300d7bd88b8e10c8d5ac99a657f517943f9af
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-01-10 19:23:26 +0600

    decoded json

--- Go_lang_HTTP_Module/03_json_encoder_encode_details/main.go
+++ Go_lang_HTTP_Module/03_json_encoder_encode_details/main.go
@@ -11,28 +11,42 @@ import (
 
 //!  json encoder encode details
 
+//? start success handler function
  func successHandler (w http.ResponseWriter ,  r *http.Request){
 
+	// dec content type json formate 
 	w.Header().Set("content-type" , "application/json")
+
+	// confirm browser request all okey
 	w.WriteHeader(http.StatusOK)
 
+
+	// create a go map  including dynamic 3 kay
 	res:= map[string]any{
 		"ok":true ,
 		"message":"json encoded successfully and all complete",
 		"dateTime":time.Now().UTC(),
 	}
 
+	// convert json and send data to browser
 	_ = json.NewEncoder(w).Encode(res)
 
 
  }
 
+ //? End success handler function
+ 
+
 
 func main() {
-
+// hit path to run successHandler function
 	http.HandleFunc("/ok" , successHandler)
 
+
+	// run server port number 8080
 	err:= http.ListenAndServe(":8080",nil)
+
+	// print err
 	fmt.Println(err)
 
 }
--- Go_lang_HTTP_Module/04_json_decoder_decod_details/main.go
+++ Go_lang_HTTP_Module/04_json_decoder_decod_details/main.go
@@ -8,32 +8,40 @@ import (
 	"time"
 )
 
+//? start helper function
  func writeJSON(w http.ResponseWriter ,status int  ,data any ){
+	// set response header
 	w.Header().Set("content-type" , "application/json")
+	//  add status code
 	w.WriteHeader(status )
+	// convert json formate 
 	_ = json.NewEncoder(w).Encode(data)
 
  }
 
+//? end helper function
+
 
  type TestRequest struct {
 	Name string `json:"name"`
-
  }
 
 //  all function here
- func testHandler ( w http.ResponseWriter ,r *http.Request){
 
+//  here 5 important work  in function
+ func testHandler (w http.ResponseWriter ,r *http.Request){
+
+
+	// method filtering 
 	if r.Method != http.MethodPost{
 		writeJSON(w,http.StatusMethodNotAllowed  , map[string]any{
 			"ok":false,
 			"error": "Only post in allowed",
 		})
-
 		return 
-
 	}
 
+	// work finished to close pipeline using defer
 	defer  r.Body.Close()
 
 
@@ -50,7 +58,7 @@ import (
 	}
 
 	req.Name= strings.TrimSpace(req.Name)
-
+ 
 	if req.Name==""{
 		writeJSON(w, http.StatusBadRequest,map[string]any{
 			"ok":false,
