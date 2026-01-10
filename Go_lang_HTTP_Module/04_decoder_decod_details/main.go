69b46d46340776f988c5ecdbe18c81f611bfd58d
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-01-10 15:58:58 +0600

    update decoder details

--- Go_lang_HTTP_Module/04_decoder_decod_details/main.go
+++ Go_lang_HTTP_Module/04_decoder_decod_details/main.go
@@ -1,5 +1,60 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+ func writeJSON(w http.ResponseWriter ,status int  ,data any ){
+	w.Header().Set("content-type" , "application/json")
+	w.WriteHeader(status )
+	_ = json.NewEncoder(w).Encode(data)
+
+ }
+
+
+ type TestRequest struct {
+	Name string `json:"name"`
+
+ }
+
+//  all function here
+ func testHandler ( w http.ResponseWriter ,r *http.Request){
+
+	if r.Method != http.MethodPost{
+		writeJSON(w,http.StatusMethodNotAllowed  , map[string]any{
+			"ok":false,
+			"error": "Only post in allowed",
+		})
+
+		return 
+
+	}
+
+	defer  r.Body.Close()
+
+
+ 	var  req  TestRequest
+	dec:=json.NewDecoder(r.Body)
+
+	if err:=dec.Decode(&req); err !=  nil {
+		writeJSON(w, http.StatusBadRequest,map[string]any{
+			"ok":false ,
+			"error" :"Invalid json formate",
+		})
+
+		return 
+	}
+
+ }
 func main() {
+
+	http.HandleFunc("/test" ,testHandler)
+
+
+
+	err := http.ListenAndServe(":5000", nil)
+	fmt.Println(err)
 
 }
