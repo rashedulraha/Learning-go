f7e76789379f48a52328eb82c199e0d37e578fa6
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-01-11 15:15:02 +0600

    ading notes types

---
+++ Go_lang_notes_CRUD/internal/notes/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Notes struct {
+	Id primitive.ObjectID `bson:"_id" json:"id"`
+	Title string 					`bson:"title" json:"title"`
+	Content string 				`bson:"content" json:"content"`
+	Pinned bool 					`bson:"pinned" json:"pinned"`
+	CreateAt  time.Time		`bson:"createAt" json:"createAt"`
+	UpdateAt time.Time 		`bson:"updateAt" json:"updateAt"`
+}
+
+type createNoteRequest struct {
+	 Title 		string 			`json:"title" binding:"required"`
+	 Content 	string 			`json:"content" binding:"required"`
+	 Pinned 	bool 				`json:"pinned" binding:"required"`
+	 
+
+}
+
+func main() {
+
+}
