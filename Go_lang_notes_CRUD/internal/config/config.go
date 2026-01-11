35654dfdea5d7b8d84a20f965ac677cf5ba83af8
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-01-11 10:18:24 +0600

    get env file and reusable function

--- Go_lang_notes_CRUD/internal/config/config.go
+++ Go_lang_notes_CRUD/internal/config/config.go
@@ -1,5 +1,42 @@
-package main
+package config
 
-func main() {
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type config struct {
+	MongoURI   string
+	MongoDB    string
+	ServerPORT string
+}
+
+// func config() {
+
+// },
+
+ func extractEnv (key string) (string  , error){
+	 value := os.Getenv(key)
+
+	  if value == ""{
+			return  "",fmt.Errorf("Missing req env")
+		}
+
+		return  value , nil
+ }
+
+
+func Load() (config, error) {
+
+
+	//  godotenv.load() reads .env and sets them into process env 
+	//  os.getenv -> reads those values 
+
+	if err :=  godotenv.Load() ; err != nil {
+		return  config{},fmt.Errorf("Failed to load .env file")
+	}
+	
 
 }
