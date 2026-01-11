efa349a0b59ef3249b1202bfc89cd1b5e96affee
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-01-11 10:29:41 +0600

    get env file key values

--- Go_lang_notes_CRUD/internal/config/config.go
+++ Go_lang_notes_CRUD/internal/config/config.go
@@ -13,9 +13,9 @@ type config struct {
 	ServerPORT string
 }
 
-// func config() {
+func config() {
 
-// },
+},
 
  func extractEnv (key string) (string  , error){
 	 value := os.Getenv(key)
@@ -37,6 +37,26 @@ func Load() (config, error) {
 	if err :=  godotenv.Load() ; err != nil {
 		return  config{},fmt.Errorf("Failed to load .env file")
 	}
+
+	// connect  and get mongodb uri 
+	mongoURI , err := extractEnv("MONGO_URI")
+	if err != nil {
+		return  config{},err
+	}
+
+	// connect and get mongodb name 
+	mongoDB , err := extractEnv("MONGO_DB_NAME")
+	if err!= nil {
+		return  config{}, err
+	}
+	
+	// connect and get port 
+	PORT , err := extractEnv("PORT")
+	if err!= nil {
+		return  config{}, err
+	}
+
+	return  config{MongoURI: mongoURI , MongoDB: mongoDB, ServerPORT: PORT}, nil
 	
 
 }
