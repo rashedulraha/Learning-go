df70247711d7adfbd60d7b8882a9b268c99ab319
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-01-12 23:39:50 +0600

    Constants

---
+++ Learning-Go-1/A_tour_of_Go/Constants/main.go
@@ -0,0 +1,41 @@
+package main
+
+// const pi = 3.14159
+
+// const appName = "Go Learning App"
+
+// const isProduction = true
+
+// const (
+// 	maxUser = 100
+// 	minAge  = 18
+// 	site    = "example.com"
+// )
+
+// const x = 10        // untyped
+// const y int = 10   // typed
+
+// const pi = 3.14
+
+
+func main() {
+
+	// fmt.Println(pi)
+	// // pi = 3.14   error (not change )
+
+	// 	fmt.Println(appName)
+
+
+	// 	if isProduction {
+	// 	fmt.Println("Live Mode")
+	// }
+
+	// var a int = x     //  allowed
+	// var b int64 = x   //  allowed
+
+
+	// const greeting = "Hello Go"
+	// fmt.Println(greeting)
+
+
+}
