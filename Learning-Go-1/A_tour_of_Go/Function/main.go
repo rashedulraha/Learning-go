7042ff4f43188942f1a4e29ea8a455c724c201c2
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-01-12 11:55:56 +0600

    multiple result function

--- Learning-Go-1/A_tour_of_Go/Function/main.go
+++ Learning-Go-1/A_tour_of_Go/Function/main.go
@@ -2,10 +2,27 @@ package main
 
 import "fmt"
 
-func add(x int, y int) int {
-	return x + y
-}
+// func add(x int, y int) int {
+// 	return x + y
+// }
+
+//!  shortened Function
+// func add (x , y int)int{
+// 	return x+y
+// }
+
+//! multiple result
+ func swap (x  , y string)(string ,  string){
+	return  x, y
+
+ }
+
+
+
+
 
 func main() {
-	fmt.Println(add(5,8))
+	// fmt.Println(add(5,56))
+	 a,b := swap("Hello" , "World")
+		fmt.Println(a,b)
 }
