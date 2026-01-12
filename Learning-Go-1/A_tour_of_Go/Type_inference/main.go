44ab1aca9b5ecd4db2b6fcdca8afbb0edce0c592
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-01-12 23:34:58 +0600

    type interface

---
+++ Learning-Go-1/A_tour_of_Go/Type_inference/main.go
@@ -0,0 +1,20 @@
+package main
+
+func main() {
+
+	// i := 42
+
+	// var i int
+	// j := i
+
+	// i := 42
+	// f := 3.142
+	// g := 0.867 + 0.5i
+
+	// v := 1
+
+	// v := 1 + 2i
+
+	// var x float32 = 3.14
+
+}
