49ed78b46b84ec450a33a60bc70ee6d663c75732
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-01-12 23:29:12 +0600

    convert flooting

---
+++ Learning-Go-1/A_tour_of_Go/Type_Conversion/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+var i int = 42
+var f float32 = float32(i)
+var u uint = uint(f)
+
+
+//  old phone price 
+var onePlus = 1000
+	
+
+
+func main() {
+ newPrice := float64(onePlus)
+ newPrice = 999.99
+		
+
+	fmt.Println(onePlus)
+	fmt.Println(newPrice)
+	// fmt.Println(f)
+	// fmt.Println(u)
+
+}
